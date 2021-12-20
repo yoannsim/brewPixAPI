72a5cd9f48b9ca5d13d83805239b50bb5207d90d
yoannsim <yoann.simonet@heig-vd.ch>
2021-12-20 18:52:59 +0100

    html API

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const request = require('request');
 
 app.use(cors())
 
-var port = normalizePort(process.env.PORT || 3000 );
+var port = process.env.PORT || 8080 ;
 console.log('running the app on port '+ port);
 app.set('port', port);
 app.set('env',"production");
 
-app.get('/', (req, res) => {
+app.get('/stock', (req, res) => {
   
 	request('https://docs.google.com/spreadsheets/d/e/2PACX-1vSm3f88Eqr8_9JLPcV3zM4IsNjCWFQLw5aMxJO1nbVUDqI2QNRqt-F1N_T7NdHO-5yixFd98_6olVUp/pub?output=csv',function (err, response, body) {
 	res.send(body);
@@ -20,6 +20,11 @@ app.get('/', (req, res) => {
   
 });
 
+app.get('/', (req, res) => {
+  
+	res.sendFile(path.join(__dirname, '/API.html'));
+ });
+  
 app.listen(port, () => {
   console.log(`api app listening at http://localhost:${port}`)
 })
